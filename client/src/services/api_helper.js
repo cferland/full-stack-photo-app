892c6bea8e20e8e25911a54e6a5e417adc1e7f86
Aladdin Omar <Aladdin.omar18@gmail.com>
2020-01-16 12:10:16 -0500

    added api helper component inside a services folder

---
+++ client/src/services/api_helper.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: "http://localhost:3001"
+})
+
+export const updatePost = async (id, updateData) => {
+    const resp = await api.put(`/post/${id}`, updateData);
+    return resp.data;
+}
+
+export const showPost = async (id) => {
+    const resp = await api.get(`/posts/${id}`);
+    return resp.data;
+  }
+
+  export const deletePost = async (id) => {
+      const resp = await api.delete(`posts/${id}`);
+      return resp.data;
+  }
+
+export const loginUser = async (loginData) => {
+    const resp = await api.post('/auth/login', loginData);
+    localStorage.setItem('authToken', resp.data.token);
+    api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+    return resp.data.user;
+}
+
+export const registerUser = async (registerData) => {
+    const resp = await api.post('/auth/register', registerData);
+    localStorage.setItem('authToken', resp.data.token);
+    api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+    return resp.data.user;
+}
+
+export const verifyUser = async () => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+        api.defaults.headers.common.authorization = `Bearer ${token}`;
+        const resp = await api.get('/auth/verify');
+        return resp.data;
+    }
+    return false;
+}
