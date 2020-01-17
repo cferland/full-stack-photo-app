9daea3d790f1c4003a8346cbc907bf535b5b0075
asrahman97 <asrahman97@gmail.com>
2020-01-17 10:27:37 -0500

    began componentDidMount

---
+++ client/src/components/gallery.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Gallery extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      post: [],
+      apiDataLoaded: false
+    }
+  }
+
+  componentDidMount = async e => {
+    const response = await axios.get('http://localhost:3000/posts')
+    console.log(response.data);
+    
+  }
+
+}
