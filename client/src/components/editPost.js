d07ef415d0b3812093c91568f6dd54710f92da17
Aladdin Omar <Aladdin.omar18@gmail.com>
2020-01-17 10:25:21 -0500

    [editPost.js] added functionality and forms for editing a post

---
+++ client/src/components/editPost.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+
+export default class EditPostForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: "",
+            image_url: "",
+            caption: "",
+            location: ""
+        }
+    }
+
+    componentDidMount() {
+        this.setFormData();
+
+    }
+     setFormData = () => {
+        if (this.props.posts.length) {
+            const { name, image_url, description, fun_fact } = this.props.posts.find(post => {
+                return post.id === parseInt(this.props.postlId)
+            })
+            this.setState({
+                name,
+                image_url,
+                description,
+                fun_fact
+            })
+        }
+    }
+
+    handleChange = (e) => {
+        let { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={(e) => {e.preventDefault(); this.props.editPost(this.props.postId, this.state)
+                this.props.setPost(this.state) 
+                }}>
+                    <input
+                        type="textarea"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                        placeholder="Username"
+                    />
+
+                    <input
+                        type="textarea"
+                        name="image_url"
+                        value={this.state.image_url}
+                        onChange={this.handleChange}
+                        placeholder="Image"
+                    />
+
+                    <input
+                        type="textarea"
+                        name="caption"
+                        value={this.state.caption}
+                        onChange={this.handleChange}
+                        placeholder="Caption"
+                    />
+
+                    <input
+                        type="textarea"
+                        name="location"
+                        value={this.state.location}
+                        onChange={this.handleChange}
+                        placeholder="Location"
+                    />
+
+                    <input type="submit" />
+                </form>
+            </div>
+        )
+    }
+}
