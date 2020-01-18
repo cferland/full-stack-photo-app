a422c20da4fbc79483ad82ca3be943b40c8da4ab
Aladdin Omar <Aladdin.omar18@gmail.com>
2020-01-18 00:38:10 -0500

    imported sass to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Link, Redirect } from 'react-router-dom';
 import './App.css';
+import './sass/app.scss';
 
 import { loginUser, registerUser, verifyUser, showPost, showPosts, createPost, updatePost, deletePost } from './services/api_helper';
 
