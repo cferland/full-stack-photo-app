067e9b8d118ee88a886b7e768bd526e3f41d7d60
Aladdin Omar <Aladdin.omar18@gmail.com>
2020-01-21 21:59:58 -0500

    changed localhost to heroku link

--- client/src/services/api_helper.js
+++ client/src/services/api_helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: "http://immense-stream-80916.herokuapp.com/"
   //put heroku link here after deploying when ready
 })
 
